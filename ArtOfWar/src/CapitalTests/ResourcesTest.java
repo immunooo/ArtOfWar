08f82de6e0ac6b8496e02fbf9ca539480980f206
Hayden-MP <hmpowers86@gmail.com>
2020-12-02 18:54:43 -0800

    Test cases for Capital package

---
+++ ArtOfWar/src/CapitalTests/ResourcesTest.java
@@ -0,0 +1,73 @@
+package CapitalTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Capital.Resources;
+
+class ResourcesTest {
+	
+	static final Resources[] RESOURCE_OBJECTS = {	new Resources(10, 100),
+													new Resources(1, 0),
+													new Resources(-1, 0),
+													new Resources(-10, -20),
+													new Resources(2000, 2001),
+													new Resources(-2001, -2000)};
+	static final int[] GOLD_VALUES = { 10, 1, -1, -10, 2000, -2001};
+	static final int[] FOOD_VALUES = { 100, 0, 0, -20, 2001, -2000};
+	
+	@Test
+	void testDefaultConstructor() {
+		Resources test = new Resources();
+		assertEquals("Default values do not match", 100, test.getFood());
+		assertEquals("Default values do not match", 100, test.getGold());
+	}
+
+	@Test
+	void testFullConstructor() {
+		for(int i = 0; i < RESOURCE_OBJECTS.length; i++) {
+			assertEquals("Constructor values do not match", GOLD_VALUES[i], RESOURCE_OBJECTS[i].getGold());
+			assertEquals("Constructor values do not match", FOOD_VALUES[i], RESOURCE_OBJECTS[i].getFood());
+		}
+	}
+
+	@Test
+	void testGetGold() {
+		for(int i = 0; i < RESOURCE_OBJECTS.length; i++) {
+			assertEquals("Gold values do not match", GOLD_VALUES[i], RESOURCE_OBJECTS[i].getGold());
+		}
+	}
+
+	@Test
+	void testSetGold() {
+		Resources test = new Resources();
+		
+		for(int i = 0; i < GOLD_VALUES.length; i++) {
+			test.setGold(GOLD_VALUES[i]);
+			int expected = GOLD_VALUES[i];
+			assertEquals("Set gold does not match", expected, test.getGold());
+		}
+		
+	}
+
+	@Test
+	void testGetFood() {
+		for(int i = 0; i < RESOURCE_OBJECTS.length; i++) {
+			assertEquals("Food values do not match", FOOD_VALUES[i], RESOURCE_OBJECTS[i].getFood());
+		}
+	}
+
+	@Test
+	void testSetFood() {
+		Resources test = new Resources();
+		
+		for(int i = 0; i < FOOD_VALUES.length; i++) {
+			test.setFood(FOOD_VALUES[i]);
+			int expected = FOOD_VALUES[i];
+			assertEquals("Set gold does not match", expected, test.getFood());
+		}
+	}
+
+}
